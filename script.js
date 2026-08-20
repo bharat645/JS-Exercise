13e49abcdc20c9228b415e8490eeb0b5f55333e2
bharat645 <Bharatnetsquare@gmail.com>
2026-08-20 16:15:17 +0530

    Add initial HTML and JavaScript files for exercise setup

---
+++ script.js
@@ -0,0 +1,171 @@
+// Exercise 1 — Console Practice
+// console.log("Welcome to JavaScript")
+// console.log("My name is Bharat")
+// console.log("I am learning JavaScript")
+// console.log("Java Script is a progromming language")
+// console.log("I will become a frontend developer")
+
+
+// Exercise 2 — Variables & Console :
+
+
+// 1 var firstName
+
+// var firstName = " Bharat";
+// var lastName = " Singh";
+// var age = " 25";
+// var city ="Ludhiana";
+// var country =" India"
+
+
+// console.log("First Name:", firstName);
+// console.log("Last Name:", lastName);
+// console.log("Age:", age);
+// console.log("City:", city);
+// console.log("Country:", country);
+
+
+// // 2 var first_name
+
+// var first_name = " Bharat";
+// var last_name = " Singh";
+// var age = " 25";
+// var city ="Ludhiana";
+// var country =" India"
+
+
+// console.log("First Name:", firstName);
+// console.log("Last Name:", lastName);
+// console.log("Age:", age);
+// console.log("City:", city);
+// console.log("Country:", country);
+
+
+// // 3 var first_name
+
+// var first_name = " Bharat";
+// var last_name = " Singh";
+// var age = " 25";
+// var city ="Ludhiana";
+// var country =" India"
+
+
+// console.log("First Name:", firstName);
+// console.log("Last Name:", lastName);
+// console.log("Age:", age);
+// console.log("City:", city);
+// console.log("Country:", country);
+
+
+// Exercise 3 — Identifier Detective
+
+// valid 
+// var firstName = " Rahul";
+// var first_name = " Ajay";
+// var firstName2 = " Ram";
+// var $firstName = " Bharat";
+// var  FIRSTNAME= " Ajay";
+
+// Not Valid
+
+// var first Name = " Rahul";   
+// (Space are not allow)
+
+// var first-name = " Ajay";
+// (hyphen(-) are not allow )
+
+// var 23firstName = " Ram";
+// (identifier name cannot start with number)
+
+// var @firstName = " Bharat";
+// (At symbol(@) are not allow)
+
+// var  FIRSTNAME= " Ajay";
+
+// var let = " Bharat";
+// (Keyword cannot define the identifier name)
+
+
+// Exercise 4 — Case Sensitivity Challenge
+
+// var name = "Bharat";
+// var Name = "Jashan";
+// var NAME = "Singh";
+// var nAmE = "Developer";
+
+// console.log(name);
+// console.log(Name);
+// console.log(NAME);
+// console.log(nAmE);
+
+// 1. Are these four variables the same ?
+//     Ans No these 4 variables are different because javaScript are Case Sensitivite.
+  
+// 2. What will happen if you write:
+// console.log(name);
+// console.log(Name);
+// console.log(NAME);
+// console.log(nAmE);
+
+// Ans  javaScript are Case Sensitivite. Each variable will print its own value. 
+
+
+// 3. What does this tell you about JavaScript identifiers?
+
+// Ans JavaScript is case-sensitive, so identifiers with different uppercase and lowercase letters are treated as different identifiers
+
+// Example 
+// var city = "Ludhiana";
+// var City = "Mohali";
+// var CITY = "Chandigarh";
+
+// console.log(city)
+// console.log(City)
+// console.log(CITY)
+
+
+// Exercise 5 — Student Profile Challenge
+
+
+
+// Student Information
+var firstName = "Bharat";
+var lastName = "Kumar";
+var age = 21;
+var gender = "Male";
+
+// Education Information
+var college = "CGC";
+var course = "BCA";
+
+// Location Information
+var city = "Abohar";
+var country = "India";
+
+// Student Profile
+console.log("=====================================");
+console.log("       STUDENT PROFILE");
+console.log("=====================================");
+
+console.log("First Name:", firstName);
+console.log("Last Name:", lastName);
+console.log("Age:", age);
+console.log("Gender:", gender);
+console.log("College:", college);
+console.log("Course:", course);
+console.log("City:", city);
+console.log("Country:", country);
+
+console.log("==============================");
+
+
+
+
+  
+
+
+
+
+
+
+
